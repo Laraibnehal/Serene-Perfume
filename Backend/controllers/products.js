b8bf58034bd8e0fcbe0375a5c8a748dcfb413dee
laraib <95610027+Laraibnehal@users.noreply.github.com>
2024-07-21 12:23:36 +0530

    Update products.js

--- Backend/controllers/products.js
+++ Backend/controllers/products.js
@@ -22,15 +22,15 @@ const getAllProducts = async(req,res) => {
     apiData = apiData.select(selectFix)
     }
     //pagination
-let page = Number(req.query.page) || 1;
-let limit =Number(req.query.limit) || 6
-let skip = (page-1)*limit;
-// page=2
-// limit=3
-// skip= 1*3 = 3
-// apiData.skip(3).limit(3);{skip first 3 data}
-apiData = apiData.skip(skip).limit(limit);
-    console.log(queryObject)
+// let page = Number(req.query.page) || 1;
+// let limit =Number(req.query.limit) || 6
+// let skip = (page-1)*limit;
+// // page=2
+// // limit=3
+// // skip= 1*3 = 3
+// // apiData.skip(3).limit(3);{skip first 3 data}
+// apiData = apiData.skip(skip).limit(limit);
+//     console.log(queryObject)
 
 const Products = await apiData   
     res.status(200).json({Products});
